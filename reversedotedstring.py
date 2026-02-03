b0ae5cf0a1ebcf3da8ea4d45e9adf928947b2668
Balaji Gaikwad <gaikwadb602@gmail.com>
2026-02-03 19:32:28 +0530

    3-2-2026

--- reversedotedstring.py
+++ reversedotedstring.py
@@ -1,6 +1,7 @@
 class Solution:
     def reverseWords(self, s):
         parts=s.split(".")
+        l=[]
         words = [word for word in parts if word != ""]
         words.reverse()
         return ".".join(words)
